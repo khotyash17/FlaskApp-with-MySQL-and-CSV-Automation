3e52e688cd78be308d86e1e4fc4105b7128aee32
Yash <yash17@gmail.com>
2025-08-29 07:20:15 +0000

    update

--- corn/export.sh
+++ corn/export.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
+#!/bin/sh
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-OUTPUT_FILE="/data/students_$TIMESTAMP.csv"
-
-mysql -h db -u root -p password studentsdb -e "SELECT * FROM students" \
-  --batch --skip-column-names | sed 's/\t/,/g' > $OUTPUT_FILE
-
-echo "[$(date)] Exported data to $OUTPUT_FILE"
+mysql -h db -u root -ppassword --ssl=0 studentsdb -e "SELECT * FROM students;" \
+  | sed 's/\t/,/g' > /data/students_$TIMESTAMP.csv
+echo "[$(date)] Exported data to /data/students_$TIMESTAMP.csv"
