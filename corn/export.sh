7b3a2483251abcce8a1bce16b83698e6e984efed
Yash <yash17@gmail.com>
2025-08-29 12:02:34 +0000

    Remove .env file with secrets

--- corn/export.sh
+++ corn/export.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+FILE="/data/students_$TIMESTAMP.csv"
+
+# Export mysql data to CSV
 mysql -h db -u root -ppassword --ssl=0 studentsdb -e "SELECT * FROM students;" \
-  | sed 's/\t/,/g' > /data/students_$TIMESTAMP.csv
-echo "[$(date)] Exported data to /data/students_$TIMESTAMP.csv"
+  | sed 's/\t/,/g' > "$FILE"
+echo "[$(date)] Exported data to $FILE"
+
+# Upload to S3
+aws s3 cp "$FILE" s3://students-reg-csv/
+
+echo "Uploaded $FILE to S3"
